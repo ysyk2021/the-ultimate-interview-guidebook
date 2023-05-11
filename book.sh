73918a3d971017f9864ed7d94532aafe46cd3465
ysykzheng <1020680508@qq.com>
2023-05-11 16:47:35 +0800

    update page size

--- book.sh
+++ book.sh
@@ -1,7 +1,7 @@
 npm install
 npx honkit epub ./ the-ultimate-interview-guidebook.epub
 
-ebook-convert the-ultimate-interview-guidebook.epub the-ultimate-interview-guidebook.pdf --no-chapters-in-toc --chapter-mark=none --subset-embedded-fonts  --pdf-page-numbers --disable-markup-chapter-headings --paper-size a5 --pdf-page-margin-bottom 36 --pdf-page-margin-left 36 --pdf-page-margin-right 36 --pdf-page-margin-top 36 --extra-css page_styles.css
+ebook-convert the-ultimate-interview-guidebook.epub the-ultimate-interview-guidebook.pdf --no-chapters-in-toc --chapter-mark=none --subset-embedded-fonts  --pdf-page-numbers --disable-markup-chapter-headings --paper-size a4 --pdf-page-margin-bottom 36 --pdf-page-margin-left 36 --pdf-page-margin-right 36 --pdf-page-margin-top 36 --extra-css page_styles.css
 
 # brew install pdftk-java
 java -jar "/usr/local/Cellar/pdftk-java/3.3.3/libexec/pdftk-all.jar" the-ultimate-interview-guidebook.pdf cat 2-end output the-ultimate-interview-guidebook-FINAL.pdf
